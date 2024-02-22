f6e85cbc8717e05c066a3e01507827c0fbcaa439
ryunchang <ckdtjq202001@gmail.com>
2024-02-22 14:51:21 +0900

    first commit

---
+++ src/main/java/com/kt/edu/thirdproject/ThirdprojectApplication.java
@@ -0,0 +1,19 @@
+package com.kt.edu.thirdproject;
+
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.ComponentScan;
+
+@EnableEncryptableProperties
+@SpringBootApplication
+@ComponentScan("com.kt")
+public class ThirdprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThirdprojectApplication.class, args);
+	}
+
+}
+
